3834a14f6415d5e0750b4260b99d86927a157ccd
Michel Uncini <uncini.michel@gmail.com>
2014-05-03 16:41:05 +0100

    Added shell script

---
+++ reset.sh
@@ -0,0 +1,13 @@
+CWD=$(pwd)
+REPO=$(git rev-parse --show-toplevel)
+
+cd "$REPO";
+
+npm install -g bower grunt-cli
+bower install
+bundle install
+
+# this should install selenium for protractor:
+node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update
+
+cd "$CWD"
--- scripts/reset.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-
-npm install --loglevel error
-bower install -q
-bundle install
-
-# this should install selenium for protractor:
-node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-managerwebdriver-manager update
